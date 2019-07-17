d0a995ced3dc5d02c4afa5975568ef31a1fe538a
Satoshi Suda <sato.robo.0729@gmail.com>
2019-07-18 00:19:28 +0900

    Introduced unary, "+" and "-"

--- 9cc.c
+++ 9cc.c
@@ -45,6 +45,7 @@ struct Node {
 };
 
 Node *term();
+Node *unary();
 Node *expr();
 Node *mul();
 
@@ -163,14 +164,22 @@ Node *term(){
   return new_node_num(expect_number());
 }
 
-Node *mul(){
-  Node *node = term();
+Node *unary() {
+  if (consume('+'))
+    return term();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), term());
+  return term();
+}
+
+Node *mul() {
+  Node *node = unary();
   
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, term());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, term());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
